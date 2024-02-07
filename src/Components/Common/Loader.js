d0b9f1a98c11ed08939b034384bc7accfe0df9d7
yk33cliff <busikuwycliff@gmail.com>
2024-02-07 14:55:14 +0300

    fixes for 7/02/2024

--- src/Components/Common/Loader.js
+++ src/Components/Common/Loader.js
@@ -1,17 +1,68 @@
 import React from "react";
 
-export default function Loader() {
+export function Loader() {
   return (
     <div className="text-center">
       {/* LOADEAR */}
       <div>
-        <img
-          src="../assets/img/loader.svg"
+        <div
           className="loader-img"
-          alt="Loader"
-        />
+          style={{
+            width: "80px",
+            height: "80px",
+
+            borderRadius: "50%",
+            position: "absolute",
+            margin: "auto",
+            animation: "spin 2s linear infinite",
+            border: "4px solid rgba(81, 75, 130, 0.1)",
+            borderTopColor: "#514b82",
+            background: "transparent",
+          }}>
+          {/* Loader */}
+          {/* Your loader content goes here */}
+          {/* End Loader */}
+        </div>
       </div>
       {/* END LOADEAR */}
     </div>
   );
 }
+
+export function Loader2() {
+  return (
+    <div className="text-center">
+      {/* LOADEAR */}
+      <div>
+        <div
+          className="loader-img"
+          style={{
+            width: "fit-content",
+            fontWeight: "bold",
+            fontFamily: "sans-serif",
+            fontSize: "16px",
+            paddingBottom: "8px",
+            background:
+              "linear-gradient(currentColor 0 0) 0 100%/0% 3px no-repeat",
+            animation: "l2 2s linear infinite",
+          }}>
+          {/* Loader */}
+          Loading...
+          {/* End Loader */}
+        </div>
+      </div>
+      {/* END LOADEAR */}
+    </div>
+  );
+}
+
+// CSS animation definition
+const styles = `
+  @keyframes l2 {
+    to {
+      background-size: 100% 3px;
+    }
+  }
+`;
+
+export default Loader; // Exporting Loader as default
--- src/Pages/loans/PrincipalRepayment.jsx
+++ src/Pages/loans/PrincipalRepayment.jsx
@@ -13,6 +13,39 @@ function PrincipalRepayment() {
   const [Reports, SetReports] = useState("");
   const [sdate, setSdate] = useState("");
   const [edate, setEdate] = useState("");
+
+  const Print = () => {
+    let printContents = document.getElementById("printablediv").innerHTML;
+    const additionalContent = `
+        <div style="background-color: #f0f0f0; padding: 20px; text-align: center;">
+            <h3>SERENITY MICROFINANCE LIMITED</h3>
+            <h6>P.O.Box 310081</h6>
+            <h6>Bulindo shopping centre Building</h6>
+            <h6>
+                email: <span style="color: red;">serenitymicrofinance@gmail.com</span>
+            </h6>
+            <h6>
+                contacts: <span style="color: red;">+256771670497</span>
+            </h6>
+            <p>
+                <u>Clients' Loan slip</u>
+            </p>
+        </div>
+    `;
+
+    // Add the additional content to the beginning of the printContents
+    printContents = additionalContent + printContents;
+
+    // Store the original content of the body
+    let originalContents = document.body.innerHTML;
+
+    // Replace the body content with the printable content
+    document.body.innerHTML = printContents;
+    window.print();
+    window.location.reload();
+    document.body.innerHTML = originalContents;
+  };
+
   const getRerport = async () => {
     var data = {
       start_date: sdate,
@@ -42,161 +75,151 @@ function PrincipalRepayment() {
       <AppContainer title="Loan payments reports">
         <>
           <div className="row row-sm">
-            <div
-              className="col-sm-12  col-md-12 col-lg-12 col-xl-8 mt-xl-4 mb-4"
-              style={{
-                borderRadius: "20px",
-                padding: "0",
-                margin: "0",
-              }}>
-              <div className="card custom-card card-dashboard-calendar ">
-                <div className="card-body">
-                  <div className="col-sm-12  col-md-12 col-lg-12 col-xl-12">
-                    <form action="" onSubmit={(e) => handleSubmit(e)}>
-                      <div className="row">
-                        <div className="col-sm-12  col-md-4 col-lg-4 col-xl-4 ">
-                          <div className="col-12">
-                            <div className="row">
-                              <label className="col-4">FROM:</label>
-
-                              <span className="col-8">
-                                <DatePicker
-                                  selected={sdate}
-                                  onChange={(e) => setSdate(e)}
-                                  dateFormat="dd/MM/yyyy"
-                                  placeholderText="dd/MM/yyyy"
-                                  className="form-control text-success"
-                                />
-                              </span>
-                            </div>
-                          </div>
-                        </div>
-                        <div className="col-sm-12  col-md-4 col-lg-4 col-xl-4 ">
-                          <div className="col-12">
-                            <div className="row">
-                              <label className="col-4">TO:</label>
-
-                              <span className="col-8">
-                                <DatePicker
-                                  selected={edate}
-                                  onChange={(e) => setEdate(e)}
-                                  dateFormat="dd/MM/yyyy"
-                                  placeholderText="dd/MM/yyyy"
-                                  className="form-control text-success"
-                                />
-                              </span>
-                            </div>
-                          </div>
-                        </div>
-                        <div className="col-sm-12  col-md-4 col-lg-4 col-xl-4 ">
-                          <div className="col-md-12 ">
-                            <div className="form-group mb-0">
-                              <button
-                                type="submit"
-                                className="btn col-lg-12 col-md-12 btn-primary">
-                                submit
-                              </button>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </form>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="row square">
-              <div className="col-lg-12 col-md-12">
-                <div className="card custom-card">
+            <div className="" id="printablediv">
+              <div
+                className="col-sm-12  col-md-12 col-lg-12 col-xl-8 mt-xl-4 mb-4"
+                style={{
+                  borderRadius: "20px",
+                  padding: "0",
+                  margin: "0",
+                }}>
+                <div className="card custom-card card-dashboard-calendar ">
                   <div className="card-body">
-                    <div className="profile-tab tab-menu-heading">
-                      <nav className="nav main-nav-line p-3 tabs-menu profile-nav-line bg-gray-100">
-                        <a
-                          className="nav-link  active"
-                          data-bs-toggle="tab"
-                          href="#principal">
-                          Principal payments Report
-                        </a>
-                        <a
-                          className="nav-link"
-                          data-bs-toggle="tab"
-                          href="#interests">
-                          Interests payments Report
-                        </a>
-                        <a
-                          className="nav-link"
-                          data-bs-toggle="tab"
-                          href="#Fines">
-                          Fines report
-                        </a>
-                      </nav>
+                    <div className="col-sm-12  col-md-12 col-lg-12 col-xl-12">
+                      <form action="" onSubmit={(e) => handleSubmit(e)}>
+                        <div className="row">
+                          <div className="col-sm-12  col-md-4 col-lg-4 col-xl-4 ">
+                            <div className="col-12">
+                              <div className="row">
+                                <label className="col-4">FROM:</label>
+
+                                <span className="col-8">
+                                  <DatePicker
+                                    selected={sdate}
+                                    onChange={(e) => setSdate(e)}
+                                    dateFormat="dd/MM/yyyy"
+                                    placeholderText="dd/MM/yyyy"
+                                    className="form-control text-success"
+                                  />
+                                </span>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="col-sm-12  col-md-4 col-lg-4 col-xl-4 ">
+                            <div className="col-12">
+                              <div className="row">
+                                <label className="col-4">TO:</label>
+
+                                <span className="col-8">
+                                  <DatePicker
+                                    selected={edate}
+                                    onChange={(e) => setEdate(e)}
+                                    dateFormat="dd/MM/yyyy"
+                                    placeholderText="dd/MM/yyyy"
+                                    className="form-control text-success"
+                                  />
+                                </span>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="col-sm-12  col-md-4 col-lg-4 col-xl-4 ">
+                            <div className="col-md-12 ">
+                              <div className="form-group mb-0">
+                                <button
+                                  type="submit"
+                                  className="btn col-lg-12 col-md-12 btn-primary">
+                                  submit
+                                </button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </form>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-            {/* Row */}
-            <div className="row row-sm">
-              <div className="col-lg-12 col-md-12">
-                <div className="card custom-card main-content-body-profile">
-                  <div className="tab-content">
-                    {/* principal installments loans */}
+              <div className="row square">
+                <div className="col-lg-12 col-md-12">
+                  <div className="card custom-card">
+                    <div className="card-body">
+                      <div className="profile-tab tab-menu-heading">
+                        <nav className="nav main-nav-line p-3 tabs-menu profile-nav-line bg-gray-100">
+                          <a
+                            className="nav-link  active"
+                            data-bs-toggle="tab"
+                            href="#principal">
+                            Principal payments Report
+                          </a>
+                        </nav>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* Row */}
+              <div className="row row-sm">
+                <div className="col-lg-12 col-md-12">
+                  <div className="card custom-card main-content-body-profile">
+                    <div className="tab-content">
+                      {/* principal installments loans */}
 
-                    <div
-                      className="main-content-body tab-pane p-4 border-top-0  active"
-                      id="principal">
-                      <div className="col-sm-12  col-md-12 col-lg-12 col-xl-12 mt-xl-4">
-                        <div className="card custom-card card-dashboard-calendar pb-0">
-                          <label className="main-content-label mb-2 pt-1">
-                            principal payments reports
-                          </label>
-                          <div className="card-body">
-                            <div className="table-responsive">
-                              <table className="table card-table text-nowrap table-bordered border-top">
-                                <thead>
-                                  <tr>
-                                    <th>NO</th>
-                                    <th>Customer </th>
-                                    <th>Loan Type</th>
-                                    <th>Loan principal</th>
-                                    <th>
-                                      principal paid <br />
-                                      back
-                                    </th>
-                                    <th>
-                                      Loans Start <br />
-                                      Date
-                                    </th>
-                                  </tr>
-                                </thead>
+                      <div
+                        className="main-content-body tab-pane p-4 border-top-0  active"
+                        id="principal">
+                        <div className="col-sm-12  col-md-12 col-lg-12 col-xl-12 mt-xl-4">
+                          <div className="card custom-card card-dashboard-calendar pb-0">
+                            <label className="main-content-label mb-2 pt-1">
+                              principal payments reports
+                            </label>
+                            <div className="card-body">
+                              <div className="table-responsive">
+                                <table className="table card-table text-nowrap table-bordered border-top">
+                                  <thead>
+                                    <tr>
+                                      <th>NO</th>
+                                      <th>Customer </th>
+                                      <th>Loan Type</th>
+                                      <th>Loan principal</th>
+                                      <th>
+                                        principal paid <br />
+                                        back
+                                      </th>
+                                      <th>
+                                        Loans Start <br />
+                                        Date
+                                      </th>
+                                    </tr>
+                                  </thead>
 
-                                <tbody>
-                                  {Array.isArray(Reports) &&
-                                    Reports.map((repo, key) => (
+                                  <tbody>
+                                    {Array.isArray(Reports) &&
+                                      Reports.map((repo, key) => (
+                                        <>
+                                          <tr key={key}>
+                                            <td>{key + 1}</td>
+                                            <td>{repo.id.customer_id}</td>
+                                            <td>{repo.id.loan_type}</td>
+                                            <td>{repo.principal_invo}</td>
+                                            <td>{repo.returned_principal}</td>
+                                            <td>{repo.start_date}</td>
+                                          </tr>
+                                        </>
+                                      ))}
+                                    {!Array.isArray(Reports) && (
                                       <>
-                                        <tr key={key}>
-                                          <td>{key + 1}</td>
-                                          <td>{repo.id.customer_id}</td>
-                                          <td>{repo.id.loan_type}</td>
-                                          <td>{repo.principal_invo}</td>
-                                          <td>{repo.returned_principal}</td>
-                                          <td>{repo.start_date}</td>
+                                        <tr>
+                                          <td
+                                            colSpan={5}
+                                            className="text-danger text-center">
+                                            No records found
+                                          </td>
                                         </tr>
                                       </>
-                                    ))}
-                                  {!Array.isArray(Reports) && (
-                                    <>
-                                      <tr>
-                                        <td
-                                          colSpan={5}
-                                          className="text-danger text-center">
-                                          No records found
-                                        </td>
-                                      </tr>
-                                    </>
-                                  )}
-                                </tbody>
-                              </table>
+                                    )}
+                                  </tbody>
+                                </table>
+                              </div>
                             </div>
                           </div>
                         </div>
@@ -205,6 +228,12 @@ function PrincipalRepayment() {
                   </div>
                 </div>
               </div>
+              <button
+                classname="btn  btn-primary col-3"
+                type="button"
+                onClick={Print}>
+                print report
+              </button>
             </div>
           </div>
         </>
